8216ce812286bb74d46af61e02771887e03d812e
Andres-FGP <andres.fgonzalez120@gmail.com>
2024-07-25 11:04:57 -0500

    Add files via upload

---
+++ CoordenadasPolaresRectangulares.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+public class Main
+    
+{
+	public static void main(String[] args) {
+	    System.out.println("                             Andres Felipe Gonzalez Pardo");
+	    System.out.println("            Conversion de coordenas                 ");
+		
+	
+		System.out.println("¿Qué tipo de conversion va a realizar?");
+	    System.out.println("1. Polares a rectangulares");
+		System.out.println("2. Rectangulares a polares");
+		
+	    
+	}
+}
